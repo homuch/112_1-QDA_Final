b5429f535e474a8b764e0b49540945c3aeb31a88
homuch <homu777@gmail.com>
2023-12-28 01:41:13 +0800

    hi

--- tests/gen_tests.py
+++ tests/gen_tests.py
@@ -1,12 +1,22 @@
-with open("./tests/example_testf8.qasm", "a") as f:
-    for i in range(1024):
+with open("./tests/report_test_2.qasm", "a") as f:
+    for i in range(512):
         f.write("""
 h q[0];
 t q[0];
-cx q[0], q[1];
+cx q[0], q[2];
 h q[0];
-cx q[0], q[1];
+cx q[0], q[2];
 t q[0];
 h q[0];
 rus q[0],[0];
+                
+h q[0];
+h q[1];
+ccx q[0], q[1], q[2];
+s q[2];
+ccx q[0], q[1], q[2];
+z q[2];
+h q[0];
+h q[1];
+rus q[0],q[1],[0],[0];
         """)
